97dec0b4c44963153bbad5a2d3f15853282cdf49
Bayu aguska rahmadi <bayuaguska@gmail.com>
2024-09-30 13:13:19 +0700

    project start

---
+++ src/main/java/com/bayu/flutter_java_todo/FlutterJavaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.flutter_java_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterJavaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterJavaTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/flutter_java_todo/FlutterJavaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.flutter_java_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterJavaTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
